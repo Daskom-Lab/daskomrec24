134c2417bfd206b529d87652fa2f1e24831833f6
LostKid <52243508+DiyArc-350@users.noreply.github.com>
2024-01-18 20:08:34 +0700

    Update AdminLoginController.php
    delete comment and update code

--- app/Http/Controllers/Auth/AdminLoginController.php
+++ app/Http/Controllers/Auth/AdminLoginController.php
@@ -11,11 +11,6 @@ use Illuminate\Http\Request;
 
 class AdminLoginController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function loginForm()
     {
         return view('admin.auth.login');
@@ -27,21 +22,13 @@ class AdminLoginController extends Controller
             'ascode' => ['required'],
             'password' => ['required']
         ]);
-        
         $model = Admin::query()->where('ascod', $request->get('ascode'))->firstOrFail();
-        // dd($model);
-
-        // dd(Hash::check($request->get('password'), $model->employee_passwd));
 
         if (!Hash::check($request->get('password'), $model->password)) {
-            return back()->with('error', 'Email or Password is Incorect');
+            return back()->with('error', 'Assistant code or Password is Incorect');
         }
-
-
         Auth::guard('admin')->login($model);
-
         return redirect()->to(route('admin.dashboard'));
-        // return dd('ok');
     }
 
     public function changePass(Request $request, $id)
@@ -50,13 +37,9 @@ class AdminLoginController extends Controller
             'password'  => 'required|min:8|string',
             'confirmPassword'  => 'required|same:password',
         ]);
-
         Admin::where('id', $id)->update([
             'password' => Hash::make($request->password),
         ]);
-
-        // dd($tes);
-        //kasih konfirmasi sblm updat password
         Auth::guard('admin')->logout();
         return redirect()->route('admin.login');
     }
