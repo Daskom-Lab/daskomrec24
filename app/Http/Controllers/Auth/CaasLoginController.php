28ee130fa29e09cda140b46d8d45aab6a93efe92
LostKid <52243508+DiyArc-350@users.noreply.github.com>
2024-01-18 20:10:26 +0700

    Update CaasLoginController.php
    delete comment

--- app/Http/Controllers/Auth/CaasLoginController.php
+++ app/Http/Controllers/Auth/CaasLoginController.php
@@ -11,11 +11,6 @@ use Illuminate\Http\Request;
 
 class CaasLoginController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function loginForm()
     {
         return view('caas.auth.login');
@@ -35,7 +30,6 @@ class CaasLoginController extends Controller
             return redirect()->route('caas.dashboard'); // disesuaikan sama nama route
         }
         return redirect()->back()->with(['error' => 'NIM / Password Salah']); // disesuaikan sama nama routenye
-        // return redirect()->to(route('dashboard'));
     }
 
 
@@ -45,12 +39,9 @@ class CaasLoginController extends Controller
             'password'  => 'required|min:8|string',
             'confirmPassword'  => 'required|same:password',
         ]);
-
         DataCaas::where('id', Auth::id())->update([
             'password' => Hash::make($request->password),
         ]);
-
-        //kasih konfirmasi sblm updat password
         Auth::guard('caas')->logout();
         return redirect()->route('caas.login');
     }
