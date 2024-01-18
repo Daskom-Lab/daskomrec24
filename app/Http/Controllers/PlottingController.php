08c45199eb336acadc1d16075777094368c3cec5
LostKid <52243508+DiyArc-350@users.noreply.github.com>
2024-01-18 20:19:46 +0700

    Update PlottingController.php
    delete some comment

--- app/Http/Controllers/PlottingController.php
+++ app/Http/Controllers/PlottingController.php
@@ -9,11 +9,6 @@ use App\Http\Requests\UpdatePlottingRequest;
 
 class PlottingController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $shifts = Shift::orderBy('day', 'asc')->orderBy('start_hour', 'asc')->get();
@@ -30,69 +25,4 @@ class PlottingController extends Controller
         return view('admin.plottingan', $data);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StorePlottingRequest  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(StorePlottingRequest $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Plotting  $plotting
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Plotting $plotting)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Plotting  $plotting
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Plotting $plotting)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdatePlottingRequest  $request
-     * @param  \App\Models\Plotting  $plotting
-     * @return \Illuminate\Http\Response
-     */
-    public function update(UpdatePlottingRequest $request, Plotting $plotting)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Plotting  $plotting
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Plotting $plotting)
-    {
-        //
-    }
 }
