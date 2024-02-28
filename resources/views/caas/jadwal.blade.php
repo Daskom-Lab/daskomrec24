9c24e07acbdb1ce6027c65e7c1d419623e802c41
Didi <dhiyaisnavisa@gmail.com>
2024-02-28 16:05:22 +0700

    feat(FE pages): add pages for integration

---
+++ resources/views/caas/jadwal.blade.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/public/css/output.css" />
+    <link rel="icon" type="DLOR" href="/public/assets/homepage/LogoDlor24.jpg" />
+    <title>Venture the Deep Within</title>
+</head>
+<body class="bg-dark-dungeon bg-cover bg-fixed bg-no-repeat bg-center cursor-sword">
+     <!-- header start -->
+   <div class="container flex justify-between">
+    <!-- navbar start -->
+    <div class="sidebar fixed left-0 right-6 h-full w-[40px] bg-hijau-gelap bg-opacity-70 pr-5 pt-3 z-50 transition-all ease-in-out duration-700">
+        <div class="flex relative items-center">
+            <i class="absolute top-2 -right-4 cursor-pointer text-md" id="btn">
+                <img src="/public/assets/sidebar/navbar.png" alt="" class="h-[25px] w-[35px]"/>
+              </i>
+        </div>
+        <ul class="profile text-fivedary font-ringbearer mt-[40px] ml-3 h-full">
+            <div class="boxsidebar pb-4 m-2 text-[16px] opacity-0 pointer-events-none">
+                <div class="mx-2 mt-2  font-semibold">
+                    <h1>Account</h1> 
+                </div>
+
+                <li class="relative mt-0 p-1 list-none">
+                    <a href="changepassCaas.html" class="border-2 border-secondary hover:border-fivedary justify-center text-secondary bg-gray-dark flex w-[90%] h-9  rounded-full items-center no-underline shadow-md shadow-gray-dark hover:bg-transparent hover:shadow-green hover:text-fivedary hover:font-semibold">
+                        <span class="text-[12px] whitespace-nowrap">change password</span>
+                    </a>
+                </li>
+                <li class="relative mt-2 p-1 list-none">
+                    <a href="dashboardcaas.html" class="border-2 border-secondary hover:border-fivedary justify-center text-secondary bg-gray-dark flex w-[90%] h-9 rounded-full items-center no-underline shadow-md shadow-gray-dark hover:bg-transparent hover:shadow-green hover:text-fivedary hover:font-semibold">
+                        <span class="text-[12px] whitespace-nowrap">profile</span>
+                    </a>
+                </li>
+
+                <div class="mx-2 mt-3 font-semibold">
+                    <h1>Contact</h1>
+                </div>
+
+                <li class="relative mt-0 p-1 list-none">
+                    <a href="https://liff.line.me/1645278921-kWRPP32q/?accountId=450vflzh" class="border-2 border-secondary hover:border-fivedary justify-center text-secondary bg-gray-dark flex w-[90%] h-9 rounded-full items-center no-underline shadow-md shadow-gray-dark hover:bg-transparent hover:shadow-green hover:text-fivedary hover:font-semibold">
+                        <span class="text-[12px] whitespace-nowrap">oa line</span>
+                    </a>
+                </li>
+                <li class="relative mt-2 p-1 list-none">
+                    <a href="theAssistant.html" class="border-2 border-secondary hover:border-fivedary justify-center text-secondary bg-gray-dark flex w-[90%] h-9 rounded-full items-center no-underline shadow-md shadow-gray-dark hover:bg-transparent hover:shadow-green hover:text-fivedary hover:font-semibold">
+                        <span class="text-[12px] whitespace-nowrap">assistant</span>
+                    </a>
+                </li>
+
+                <div class="mx-2 mt-3  font-semibold">
+                    <h1>Recruitment</h1>
+                </div>
+
+                <li class="relative mt-0 p-1 list-none">
+                    <a href="kelulusan.html" class="border-2 border-secondary hover:border-fivedary justify-center text-secondary bg-gray-dark flex w-[90%] h-9 rounded-full items-center no-underline shadow-md shadow-gray-dark hover:bg-transparent hover:shadow-green hover:text-fivedary hover:font-semibold">
+                        <span class="text-[12px] whitespace-nowrap">graduation</span>
+                    </a>
+                </li>
+                <li class="relative mt-2 p-1 list-none">
+                    <a href="jadwal.html" class="border-2 border-secondary hover:border-fivedary justify-center text-secondary bg-gray-dark flex w-[90%] h-9 rounded-full items-center no-underline shadow-md shadow-gray-dark hover:bg-transparent hover:shadow-green hover:text-fivedary hover:font-semibold">
+                        <span class="text-[12px] whitespace-nowrap">schedule</span>
+                    </a>
+                </li>
+                <li class="relative mt-2 p-1 list-none">
+                    <a href="chooseroleCaas.html" class="border-2 border-secondary hover:border-fivedary justify-center text-secondary bg-gray-dark flex w-[90%] h-9 rounded-full items-center no-underline shadow-md shadow-gray-dark hover:bg-transparent hover:shadow-green hover:text-fivedary hover:font-semibold">
+                        <span class="text-[12px] whitespace-nowrap">role</span>
+                    </a>
+                </li>
+
+                <li class="relative mt-2 p-1 list-none">
+                    <a href="homepage.html" class="border-2 border-secondary hover:border-fivedary justify-center text-secondary bg-gray-dark flex w-[90%] h-9 rounded-full items-center no-underline shadow-md shadow-gray-dark hover:bg-transparent hover:shadow-merah hover:text-fivedary hover:font-semibold">
+                        <span class="text-[12px] whitespace-nowrap">log out</span>
+                    </a>
+                </li>
+            </div>
+        </ul>
+    </div>
+    <!-- navbar ends -->
+    </div>
+    <!-- header ends -->
+
+    <!-- tabel jadwal start -->
+    <div class="container relative max-w-3xl mx-auto py-2"> 
+        <div class="judul-tabel text-center mt-7 mb-5">
+            <h1 class="text-white text-[32px] font-bold font-ringbearer">Select a Schedule</h1>
+        </div>
+        <div class="flex justify-center overflow-x-auto px-3">
+            <table class="table-auto text-[12px] lg:text-[15px] tracking-wide font-ringbearer">
+                <thead class="header">
+                    <tr class="tabel bg-dark text-fivedary">
+                        <th class="border border-butter px-2 lg:px-4 py-2">no</th>
+                        <th class="border border-butter px-2 lg:px-4 py-2">shift</th>
+                        <th class="border border-butter px-2 lg:px-4 py-2">hari, tanggal</th>
+                        <th class="border border-butter px-2 lg:px-4 py-2">waktu</th>
+                        <th class="border border-butter px-2 lg:px-4 xpy-2">kuota</th>
+                        <th class="border border-butter px-2 lg:px-4 py-2">opsi</th>
+                    </tr>
+                </thead>
+                <tbody class="bg-light text-fivedary text-[10px] lg:text-[13px] table-auto">
+                    <tr>
+                        <th class="border border-butter px-2 lg:px-4 py-2">1.</th>
+                        <th class="border border-butter px-2 lg:px-4 py-2">shift 1</th>
+                        <th class="border border-butter px-2 lg:px-4 py-2">3 februari</th>
+                        <th class="border border-butter px-2 lg:px-4 py-2">06.30 - 09.00</th>
+                        <th class="border border-butter px-2 lg:px-4 py-2">12</th>
+                        <th class="border border-butter px-2 lg:px-4 py-2">
+                            <button class="bg-dark hover:bg-gray-dark px-2 py-1 rounded-md">
+                             pilih
+                            </button>
+                            </th>
+                    </tr>
+                    <tr>
+                        <th class="border border-butter px-2 lg:px-4 py-2">2.</th>
+                        <th class="border border-butter px-2 lg:px-4 py-2">shift 3</th>
+                        <th class="border border-butter px-2 lg:px-4 py-2">3 februari</th>
+                        <th class="border border-butter px-2 lg:px-4 py-2">13.30 - 15.00</th>
+                        <th class="border border-butter px-2 lg:px-4 py-2 text-butter">penuh</th>
+                        <th class="border border-butter px-2 lg:px-4 py-2">
+                            <button class="bg-gray-dark bg-blur px-2 py-1 rounded-md">
+                            pilih
+                            </button>
+                            </th>
+                    </tr>  
+                    <tr>
+                        <th class="border border-butter px-2 lg:px-4 py-2">3.</th>
+                        <th class="border border-butter px-2 lg:px-4 py-2">shift 3</th>
+                        <th class="border border-butter px-2 lg:px-4 py-2">12 februari</th>
+                        <th class="border border-butter px-2 lg:px-4 py-2">16.30 - 19.00</th>
+                        <th class="border border-butter px-2 lg:px-4 py-2">5</th>
+                        <th class="border border-butter px-2 lg:px-4 py-2">
+                            <button class="bg-dark hover:bg-gray-dark px-2 py-1 rounded-md">
+                                <a href="fixjadwal.html">
+                                    pilih
+                                </a>
+                            </button>
+                            </th>
+                    </tr>
+                    <tr>
+                        <th class="border border-butter px-2 lg:px-4 py-2">4.</th>
+                        <th class="border border-butter px-2 lg:px-4 py-2">shift 4</th>
+                        <th class="border border-butter px-2 lg:px-4 py-2">21 februari</th>
+                        <th class="border border-butter px-2 lg:px-4 py-2">06.30 - 10.00</th>
+                        <th class="border border-butter px-2 lg:px-4 py-2">8</th>
+                        <th class="border border-butter px-2 lg:px-4 py-2">
+                            <button class="bg-dark hover:bg-gray-dark px-2 py-1 rounded-md">
+                            pilih
+                            </button>
+                            </th>
+                    </tr> 
+                    <tr>
+                        <th class="border border-butter px-2 lg:px-4 py-2">5.</th>
+                        <th class="border border-butter px-2 lg:px-4 py-2">shift 4</th>
+                        <th class="border border-butter px-2 lg:px-4 py-2">26 februari</th>
+                        <th class="border border-butter px-2 lg:px-4 py-2">13.30 - 15.00</th>
+                        <th class="border border-butter px-2 lg:px-4 py-2 text-butter">penuh</th>
+                        <th class="border border-butter px-2 lg:px-4 py-2">
+                            <button class="bg-gray-dark bg-blur px-2 py-1 rounded-md">
+                            pilih
+                            </button>
+                            </th>
+                    </tr>    
+                </tbody>
+            </table>
+        </div>
+        <div class="text-white font-ringbearer mt-6 flex justify-center gap-x-6">
+            <a href="#">
+                <button class="text-[16px] bg-light hover:bg-dark px-3 py-2 rounded-sm">
+                    previous
+                </button>
+            </a>
+            <a href="#">
+                <button class="text-[16px] bg-light hover:bg-dark px-7 py-2 rounded-sm">
+                    next
+                </button >
+            </a>
+        </div>
+    </div>
+    <!-- tabel jadwal ends -->
+
+    <!-- footer start -->
+    <footer class="fixed bottom-0 left-0 right-0 mt-3 mb-3 text-center">
+        <h1 class="lg:text-[20px] font-ringbearer text-[#A37651] text-stroke sm:text-[15px] md:text-[15px]">#Venture The Deep Within
+    </footer>
+    <!-- footer ends -->
+
+   <!-- script sidebar -->
+    <script src="/public/js/sidebar.js"></script>
+</body>
+
+</html>
