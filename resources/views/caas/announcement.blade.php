ef41c032a638f5f103223ccaacfb151e66896711
LostKid <52243508+DiyArc-350@users.noreply.github.com>
2024-03-14 09:14:41 +0700

    Fix some caas feature and Replace Testing Mesaages in Views (#9)
    * modified:   .gitignore
    	update set status function
    	update the update function
    	update the update function
    	add new fillable
    	add new fillable
    	modified:   composer.lock
    	add new field
    	add new field
    	modified:   resources/views/admin/caasList.blade.php
    	modified:   resources/views/admin/dashboard.blade.php
    	modified:   resources/views/admin/roleList.blade.php
    
    * Changes to be committed:
    	Add CRUD functions
    	Fix minor typo
    	Add view for DataAdmin
    	Add routes for DataAdminController
    
    * modified:   .gitignore
    	Fix target id for change pass function
    	deleted:    app/Http/Controllers/MessageController.php
    	deleted:    app/Http/Controllers/StageController.php
    	deleted:    app/Http/Controllers/StatusController.php
    	Fix change datatype for 'nohp' field
    	modified:   resources/views/admin/caasList.blade.php
    
    * remove composer.lock
    
    * feat(FE pages): add pages for integration
    
    * build: install tailwind dependency
    
    * chore: delete unused files
    
    * feat: frontend integration
    
    * feat(caas) : add feature functions for caas
    
    * feat(caas): add routes for caas
    
    * fix: minor typo and rename file
    
    * feat: frontend integration pages
    
    * chore: update README.md
    
    * Update schedule.blade.php
    
    * feat: add middleware for role and schedule
    
    * chore: add blade syntax for table index
    
    * chore: delete unecessary files
    
    * chore: minor typo
    
    * fix: login views and error messages
    
    * chore: edit ongoing stage messages
    
    * fix: enable acces to fix-role when choose role switched off

--- resources/views/caas/announcement.blade.php
+++ resources/views/caas/announcement.blade.php
@@ -29,16 +29,16 @@
             {!! $msg->pass_msg !!}
         </p>       
         @elseif (auth()->guard('caas')->user()->status->isPass == 0)
-        <p class="announcement text-secondary text-[12px] text-justify max-w-[200px] mx-auto font-vinque">
+        <p class="announcement text-secondary text-[12px] text-justify max-w-[250px] mx-auto font-vinque">
             {!! $msg->failed_msg !!}
         </p>
         @elseif (auth()->guard('caas')->user()->announcecheck()->isMessageActive == 0 && auth()->guard('caas')->user()->status->isPass == 1)
-         <p class="announcement text-secondary text-[12px] text-center max-w-[200px] mx-auto font-vinque">
-            Tahap {{ auth()->guard('caas')->user()->status->stages->stagesName }} sedang berlangsung. Cek lagi jika OA DLOR 2024 sudah memberi notifikasi.
+         <p class="announcement text-secondary text-[12px] text-center max-w-[250px] mx-auto font-vinque">
+            <span class="text-[#4322fb] ">Tahap {{ auth()->guard('caas')->user()->status->stages->stagesName }}</span> sedang berlangsung.<br> Semangat dan pantau terus informasi di OA Line Recruitment Daskom Laboratory untuk instruksi berikutnya.
         </p>
         @endif
 
-        @if (auth()->guard('caas')->user()->announcecheck()->isPlotActive == 1 && auth()->guard('caas')->user()->status->isPass == 1)
+        @if (auth()->guard('caas')->user()->announcecheck()->isPlotActive == 1 && auth()->guard('caas')->user()->status->isPass == 1 && !boolval(auth()->guard('caas')->user()->plots))
         <a href="{{ route('caas.schedule') }}">
             <button class="button-schedule mt-[130px] mb-0 text-secondary font-semibold font-ringbearer text-[12px] bg-coklat-muda px-4 py-2 shadow-secondary shadow-xl hover:text-coklat-terang hover:bg-primary">
                 Pilih Jadwal
--- resources/views/caas/auth/login.blade.php
+++ resources/views/caas/auth/login.blade.php
@@ -9,46 +9,6 @@
     <link rel="icon" type="DLOR" href="{{ asset('/assets/caas/landing/LogoDlor24.jpg') }}" />
 </head>
 <body class="bg-welcome-dungeon bg-cover bg-fixed bg-no-repeat bg-center cursor-sword">
-    {{-- <!-- login start -->
-    <div class="container relative max-w-2xl mx-auto py-1">
-        <div class="mt-[50px] flex justify-center">
-            <img src="{{ asset('/assets/caas/caas/loginpaper.png') }}" alt="" class="h-[460px] w-[350px]">
-        </div>
-        <div class="absolute top-0 left-0 right-0 text-center mt-[110px] z-10">
-            <div class="text-black font-ringbearer -mb-[80px]">
-                <h1 class="text-[30px] font-bold">Welcome <br>Adventurer</h1> 
-            </div>
-            <form class="flex flex-col justify-around items-center h-3/5" action="{{ route('caas.login.check') }}" method="post">
-                @csrf
-                <div class="flex flex-col items-center justify-between font-vinque h-60 font-bold">
-                    <input type="text" placeholder="enter your nim" name="nim" id="nim"
-                        class="mt-[115px] w-60 h-16 text-[#514D4D] border-b-gray-700 border-b-2 bg-transparent focus:outline-none invalid:border-b-gray-500 text-sm text-center placeholder:text-[#514D4D] transition-all duration-300 ease-in-out" required />
-                        @error('nim')
-                        <div class="alert alert-danger mt-2 text-red-600 text-[11px] text-center justify-center">
-                            nim anda salah!!
-                        </div>
-                        @enderror
-                        
-                    <input placeholder="enter your password" name="password" id="password" type="password"
-                        class="mt-8 w-60 h-16 text-[#514D4D] border-b-gray-700 border-b-2 bg-transparent focus:outline-none invalid:border-b-gray-500 text-sm text-center placeholder:text-[#514D4D] transition-all duration-300 ease-in-out" required />
-                        @error('password')
-                        <div class="alert alert-danger mt-2 text-red-600 text-[11px] text-center justify-center">
-                            please 8 karakter
-                        </div>
-                        @enderror
-                                
-                    <button type="submit"
-                        class="mt-9 w-[160px] h-28 shadow-semi-sm active:shadow-in-semi-sm active:shadow-bright-sun duration-200">
-                            <h1 class="font-ringbearer font-bold flex justify-center items-center text-center text-[22px] h-[48px]">
-                                l o g i n
-                            </h1>
-                    </button>
-                </div>
-            </form>
-        </div>
-    </div>
-    <!-- login ends --> --}}
-    <!-- changepass start -->
 <div class="container relative max-w-2xl mx-auto py-1">
     <div class="mt-[50px] flex justify-center">
         <img src="{{ asset('/assets/caas/caas/loginpaper.png') }}" alt="" class="h-[460px] w-[350px]">
@@ -61,16 +21,16 @@
             @csrf            
             <div class="flex flex-col items-center justify-between font-vinque h-60 font-bold">
                 <input type="text" placeholder="enter your nim" name="nim" id="nim" value="{{ old('nim') }}" class="mt-[108px] w-60 h-16 text-graymid border-b-gray-700 border-b-2 bg-transparent focus:outline-none invalid:border-b-gray-500 text-sm text-center placeholder:text-[#514D4D] transition-all duration-300 ease-in-out" required/>  
-                @error('old_password')
+                @error('nim')
                 <div class="alert alert-danger mt-2 text-red-600 text-[11px] text-center justify-center">
-                    password anda salah!!
+                    {{ $message }}
                 </div>
                 @enderror
 
                 <input type="password" placeholder="password" name="password" id="password" value="{{ old('password') }}"  class="mt-[48px] w-60 h-16 text-graymid border-b-gray-700 border-b-2 bg-transparent focus:outline-none invalid:border-b-gray-500 text-sm text-center placeholder:text-[#514D4D] transition-all duration-300 ease-in-out" required/>
                 @error('password')
                 <div class="alert alert-danger mt-2 text-red-600 text-[11px] text-center justify-center">
-                    password minimal 8 karakter
+                    {{ $message }}
                 </div>
                 @enderror
 
@@ -81,7 +41,6 @@
         </form>
     </div> 
 </div>
-<!-- changepass end -->
     @include('caas.partials.footer')
 </body>
 </html>
